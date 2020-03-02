b04d642950158d1d6fa68d8cc82758dbeee88218
emma <sonicjane2020@gmail.com>
2020-03-02 11:10:02 -0800

    Installed react router dom and react hook form.

--- saltiest/src/App.js
+++ saltiest/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
 function App() {
   return (
     <div className="App">
-      
      <h1>ITS WORKING!!!</h1>
     </div>
   );
