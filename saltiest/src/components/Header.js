1257841f9427fd33c4ce085108db81915e51550a
emma <sonicjane2020@gmail.com>
2020-03-03 10:02:06 -0800

    Added styling to the header

--- saltiest/src/components/Header.js
+++ saltiest/src/components/Header.js
@@ -3,21 +3,54 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  NavLink
 } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 import HomePage from "./HomePage";
+import styled from 'styled-components'
+
+const HeaderDiv = styled.div`
+    display: flex;
+    background-color: #FA7921;
+`
+
+const HeaderUL = styled.ul`
+    display: flex;
+    text-decoration: none;
+    flex-basis: 75%;
+    justify-content: space-around;
+`
+
+const CuteButton = styled.button`
+    background-color: #FDE74C;
+    color: #5BC0EB;
+    width: 120px;
+    font-size: 1.4rem;
+`
+
+const FlexTest = styled.div`
+    display: flex;
+    flex-basis: 10%;
+`
+
+const StyledH1 = styled.h1`
+    color: white;
+    margin-left: 10%;
+`
 
 export default function Header(){
     return (
-        <div className="header">
-        <h1>Header</h1>
-        <ul>
-            <li><Link to="/Login">Login</Link></li>
-            <li><Link to="/Signup">Signup</Link></li>
-            <li><Link to="/HomePage">Home</Link></li>
-        </ul>
-        </div>
+        <HeaderDiv className="header">
+            <div className="headerText">
+                <StyledH1>TrollFindr</StyledH1>
+            </div>
+        <HeaderUL>
+            <CuteButton><Link to="/Login">Login</Link></CuteButton>
+            <CuteButton><Link to="/Signup">Signup</Link></CuteButton>
+            <CuteButton><Link to="/HomePage">Home</Link></CuteButton>
+        </HeaderUL>
+        </HeaderDiv>
     )
 }
--- saltiest/src/components/HomePageCard.js
+++ saltiest/src/components/HomePageCard.js
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from 'styled-components'
+
+const HomePageDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
 
 const CharacterCard = props => {
     return (
-        <div className = "HomePageCard" key>
+        <HomePageDiv className = "HomePageCard" key>
         <h2>{props.name}</h2>
         <p>{props.age}</p>
-        </div>
+        </HomePageDiv>
     )
 }
 export default CharacterCard;
